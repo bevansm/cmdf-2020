2eef9e57955c854a661ab47cbdf24a8b9403840b
dzh-ad <zhouhang.amelia@gmail.com>
2020-03-07 18:41:39 -0800

    Updated text in Intro + Concepts

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -3,11 +3,14 @@ import {Typography } from "antd";
 import { PageEnum } from "../../constants/PageEnum";
 import { PageTemplate } from "../shared/PageTemplate"
 
+const { Title } = Typography;
 const { Text } = Typography;
 
 export class Introduction extends Component {
   render() {
     return <PageTemplate nextPage={PageEnum.CONCEPTS} title="Introduction">
+        <Title level={2}>Introduction</Title>
+        
         <Text>This is a placeholder.</Text>
     </PageTemplate>;
   }
