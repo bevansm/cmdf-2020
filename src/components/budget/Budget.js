8491e2266b841bb57b11b23f77acd24d212cfad4
m <mbe_pro@outlook.com>
2020-03-07 17:45:10 -0800

    Beginning budget component

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import { Typography } from "antd";
+import {Popover, Typography} from "antd";
 import { PageEnum } from "../../constants/PageEnum";
 import { PageTemplate } from "../shared/PageTemplate"
+import {BudgetPopoverContent} from "./BudgetPopoverContent";
 
 const { Text } = Typography;
 
 export class Budget extends Component {
     render() {
         return <PageTemplate nextPage={PageEnum.PLAN} title="Weekly Budget">
+            <Popover content={<BudgetPopoverContent/>}>
+
+            </Popover>
             <Text>This is a placeholder.</Text>
         </PageTemplate>;
     }
---
+++ src/components/budget/BudgetPopoverContent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {Typography} from "antd";
+
+const { Text } = Typography;
+
+export class BudgetPopoverContent extends Component {
+    render() {
+        return <Text>This is a placeholder.</Text>;
+    }
+}
