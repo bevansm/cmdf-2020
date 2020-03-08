ed579337a508f6a74cd8477d64475585be798fd8
amymuwu <cheeseicles@gmail.com>
2020-03-08 10:02:44 -0700

    added quiz questions

--- src/constants/Quizes.js
+++ src/constants/Quizes.js
@@ -1,10 +1,91 @@
 export const quizzes = [
     {
-        question: "This is a sample quiz.",
-        link: "https://github.com/mebird",
-        options: ["A", "B", "C", "D"],
-        correctOption: "A",
+        question: "How much (in percent) is the PST and GST combined? (Note: this is also called HST (harmonized sale tax)",
+        options: ["11%", "12%", "13%", "14%"],
+        correctOption: "12%",
+        rightPoints: 3,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is an example of a  “need”?",
+        link: "https://blog.cypruscu.com/teen-financial-toolkit-wants-vs.-needs",
+        options: ["Toys", "Water", "Laptop", "Candy"],
+        correctOption: "Water",
+        rightPoints: 2,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is a “need”?",
+        link: "https://blog.cypruscu.com/teen-financial-toolkit-wants-vs.-needs",
+        options: ["A gift ", "Something that you have to have to survive or complete a task", "An item or thing you want to upgrade", "The newest products on the shelf"],
+        correctOption: "Something that you have to have to survive or complete a task", "An item or thing you want to upgrade", "The newest products on the shelf",
         rightPoints: 1,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is an example of a “want”?",
+        link: "https://blog.cypruscu.com/teen-financial-toolkit-wants-vs.-needs",
+        options: ["Food", "Shelter", "Water", "Pop"],
+        correctOption: "Pop",
+        rightPoints: 2,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is a budget?",
+        link: "https://www.mathsisfun.com/money/budget.html",
+        options: ["A plan to maximize profit", "The minimum amount of money you will spend", "The maximum money you will earn in a week", "A plan for income and spendings over time"],
+        correctOption: "A plan for income and spendings over time",
+        rightPoints: 1,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is profit?",
+        link: "https://www.youtube.com/watch?v=ksnKQpdMgL0",
+        options: ["The total amount of money you make in a day", "The amount of money made after subtracting fixed and variables costs", "The amount of money that is deducted from your earnings", "The rate of earnings growth"],
+        correctOption: "The amount of money made after subtracting fixed and variables costs",
+        rightPoints: 1,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is revenue?",
+        link: "https://www.youtube.com/watch?v=ksnKQpdMgL0",
+        options: ["Spendings", "The rate of earnings growth", "Income", "The amount of money deducted from your earnings"],
+        correctOption: "Income",
+        rightPoints: 1,
+        wrongPoints: -1
+    },
+
+    
+    {
+        question: "Which of the following is not exempt from PST?",
+        link: "https://www2.gov.bc.ca/gov/content/taxes/sales-taxes/pst/exemptions",
+        options: ["School supplies", "Unprocessed foods", "Toys", "Safety Equipment"],
+        correctOption: "Toys",
+        rightPoints: 3,
+        wrongPoints: -1
+    },
+    
+    {
+        question: "What is a fixed expense?",
+        link: "https://www2.gov.bc.ca/gov/content/taxes/sales-taxes/pst/exemptions",
+        options: ["An expense that stays the same every month", "An expense for fixing your house", "The cost of fixing broken equipment", "The cost of fixing broken electronics"],
+        correctOption: "An expense that stays the same every month",
+        rightPoints: 2,
+        wrongPoints: -1
+    },
+
+    {
+        question: "What is a variable expense?",
+        link: "https://www2.gov.bc.ca/gov/content/taxes/sales-taxes/pst/exemptions",
+        options: ["The rate of change of an expense", "The name for the math done to calculate expenses", "An expense that changes over time", "The range that an expense can be"],
+        correctOption: "An expense that changes over time",
+        rightPoints: 2,
         wrongPoints: -1
     }
 ];
